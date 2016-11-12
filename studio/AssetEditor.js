faa6e9b1edbb6ca36fca46b861e30d12347c57c6
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-11-12 14:36:44 +0100

    fix #2 display link information in asset dashboard

--- studio/AssetEditor.js
+++ studio/AssetEditor.js
@@ -19,9 +19,22 @@ export default class FileEditor extends Component {
     this.setState({ isMetaView: false, content: content })
   }
 
+  async componentDidUpdate () {
+    const { entity } = this.props
+
+    if (entity.link && (!this.state.link || this.state.link.indexOf(entity.link) === -1)) {
+      try {
+        const link = await Studio.api.get(`api/asset/link/${encodeURIComponent(entity.link)}`, { parseJSON: false })
+        this.setState({ link: link })
+      } catch (e) {
+
+      }
+    }
+  }
+
   render () {
     const { entity, onUpdate } = this.props
-    const { isMetaView, content } = this.state
+    const { isMetaView, content, link } = this.state
     const downloadUrl = Studio.resolveUrl(`api/asset/${entity.name}`)
     let mode = 'text'
     if (entity.name.includes('.js')) {
@@ -38,27 +51,28 @@ export default class FileEditor extends Component {
 
     return (isMetaView ? <div className='custom-editor'>
       <div><h1><i className='fa fa-file' /> {entity.name}</h1></div>
-      <div><i>assets extension is currently in beta</i></div>
-      <div>
-        <div>Embed into a template content, template helpers using or a custom script:
-          <code>
-            <h2 style={{textTransform: 'none'}}>
-              { '{#asset ' + entity.name + '}'}
-            </h2>
+      <p><i>assets extension is currently in beta</i></p>
 
-            <h2 style={{textTransform: 'none'}}>
-              { '{#asset ' + entity.name + ' @encoding=base64}'}
-            </h2>
-          </code>
-        </div>
-      </div>
+      {link ? <p>Asset is linked to file: {link}</p> : <div />}
+
+      <p>
+        Embed into a template content, template helpers using or a custom script:
+        <code>
+          <h3 style={{textTransform: 'none'}}>
+            { '{#asset ' + entity.name + '}'}
+          </h3>
+
+          <h3 style={{textTransform: 'none'}}>
+            { '{#asset ' + entity.name + ' @encoding=utf8|base64|javascript}'}
+          </h3>
+        </code>
+      </p>
       <div>
         <a className='button confirmation' target='_blank' href={downloadUrl}>
           <i className='fa fa-download' /> Download file
         </a>
         {entity.link
-          ? <span /> : <button className='button confirmation'
-            onClick={() => AssetUploadButton.OpenUpload()}><i className='fa fa-upload' />
+          ? <span /> : <button className='button confirmation' onClick={() => AssetUploadButton.OpenUpload()}><i className='fa fa-upload' />
           Upload file</button>}
         <button className='button confirmation' onClick={() => this.openEditor()}>
           <i className='fa fa-folder-open' /> Open in editor
