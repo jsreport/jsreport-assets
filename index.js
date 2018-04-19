7d7dd40e0003fcf0f57d761f0aae488d73429092
BJR Matos <bjrmatos@gmail.com>
2018-04-19 12:11:49 -0500

    move optionsSchema to jsreport.config.js

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ var config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
-
-  config.optionsSchema = {
-    extensions: {
-      assets: {
-        type: 'object',
-        properties: {
-          allowedFiles: { type: 'string' },
-          searchOnDiskIfNotFoundInStore: { type: 'boolean' },
-          rootUrlForLinks: { type: 'string' },
-          publicAccessEnabled: { type: 'boolean' }
-        }
-      }
-    }
-  }
-
   config.main = main
   config.directory = __dirname
   return config
