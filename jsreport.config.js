b2e31fc3e9b6382e0b0fbade664f8898ebc33828
BJR Matos <bjrmatos@gmail.com>
2019-02-18 12:51:32 -0500

    use reporter.express.exposeOptionsToApi to expose options

--- jsreport.config.js
+++ jsreport.config.js
@@ -8,7 +8,7 @@ module.exports = {
         type: 'object',
         properties: {
           allowedFiles: { type: 'string' },
-          allowAssetsLinkedToFiles: { type: 'boolean', $exposeToApi: true },
+          allowAssetsLinkedToFiles: { type: 'boolean' },
           searchOnDiskIfNotFoundInStore: { type: 'boolean' },
           rootUrlForLinks: { type: 'string' },
           publicAccessEnabled: { type: 'boolean' }
